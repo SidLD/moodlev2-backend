1ad529736c2312cd98dab18cf32c8086ae6ecaaf
Clint <clint.m@freshclinics.com.au>
2023-02-27 17:24:09 +0800

    sugad sini cid na format kun pwede salamat

--- schemas/questionSchema.js
+++ schemas/questionSchema.js
@@ -1,37 +1,39 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const questionSchema = mongoose.Schema({
-    exam:{
-        type: mongoose.Schema.Types.ObjectId,
-        ref: 'Exam'
+const logSchema = mongoose.Schema(
+  {
+    user: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "User",
+    },
+    detail: String,
+  },
+  { timestamps: true }
+);
+
+const questionSchema = mongoose.Schema(
+  {
+    exam: {
+      type: mongoose.Schema.Types.ObjectId,
+      ref: "Exam",
     },
     question: {
-        type: String,
-        required: true
+      type: String,
+      required: true,
     },
-    type:{
-        type: String,
-        enum: ["MultipleChoice", "FillInTheBlank", "TrueOrFalse"]
+    type: {
+      type: String,
+      enum: ["MultipleChoice", "FillInTheBlank", "TrueOrFalse"],
     },
-    answer:{
-        type: String
+    answer: {
+      type: String,
     },
     choices: [String],
-    log: [
-        {
-            user:{
-                type: mongoose.Schema.Types.ObjectId,
-                ref:'User'
-            },
-            detail: String,
-            createdAt: {
-                type: Date,
-                default: Date.now()
-            }
-        }
-    ]
-}, {
-    timestamps: true}
-)
+    log: [logSchema],
+  },
+  {
+    timestamps: true,
+  }
+);
 
 module.exports = mongoose.model("Question", questionSchema);
