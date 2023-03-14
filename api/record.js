8d3732eb708ff398fbdeb95d8a97b687fa09d5ec
Clint <clint.m@freshclinics.com.au>
2023-03-14 17:22:39 +0800

    fix: PUT method for records and linting

--- api/record.js
+++ api/record.js
@@ -1,102 +1,103 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
 
-const verifyToken = require("../Utilities/VerifyToken")
+const verifyToken = require("../Utilities/VerifyToken");
 const Exam = require("../schemas/examSchema");
 const User = require("../schemas/userSchema");
 const Record = require("../schemas/recordSchema");
 
+const objectId = mongoose.Types.ObjectId;
 //magamit didi _id para sa record._id
 app.get("/record", verifyToken, async (req, res) => {
-    const params = req.query;
-    Record.where(params)
-        .populate({
-            path: 'exam',
-            select: '_id dateTimeStart dateTimeEnd duration itemNumer category',
-            populate: {
-                path: 'category',
-                select: 'name _id'
-            }
-        })
-        .populate({
-            path: 'student',
-            select: '_id firstName lastName'
-        })
-        .populate({
-            path: 'answers.question'
-        })
-        .exec( async (err, data) => {
-            //array of data
-            try {
-                if(req.user.role === "admin" || req.user.role === "superadmin"){
-                    res.status(200).send({message: "Success", data: data})
-                }else{       
-                    data.forEach(record => {
-                        //an records na
-                        let answers = record.answers;
-                        answers.forEach(room => {
-                            if(room.answer === room.question.answer){
-                                answers.isCorrect == true;
-                            }else{
-                                answers.isCorrect == false;
-                            }
-                            room.question.answer = undefined;
-                            room.question.choices = undefined;
-                        })
-                    });
-                    res.status(200).send({message: "Success", data: data})
-                }
-            } catch (error) {
-                res.status(400).send({message: "Error", error: error.message})   
-            }
-        } )
-})
+  const params = req.query;
+  Record.where(params)
+    .populate({
+      path: "exam",
+      select: "_id dateTimeStart dateTimeEnd duration itemNumer category",
+      populate: {
+        path: "category",
+        select: "name _id",
+      },
+    })
+    .populate({
+      path: "student",
+      select: "_id firstName lastName",
+    })
+    .populate({
+      path: "answers.question",
+    })
+    .exec(async (err, data) => {
+      //array of data
+      try {
+        if (req.user.role === "admin" || req.user.role === "superadmin") {
+          res.status(200).send({ message: "Success", data: data });
+        } else {
+          data.forEach((record) => {
+            //an records na
+            let answers = record.answers;
+            answers.forEach((room) => {
+              if (room.answer === room.question.answer) {
+                answers.isCorrect == true;
+              } else {
+                answers.isCorrect == false;
+              }
+              room.question.answer = undefined;
+              room.question.choices = undefined;
+            });
+          });
+          res.status(200).send({ message: "Success", data: data });
+        }
+      } catch (error) {
+        res.status(400).send({ message: "Error", error: error.message });
+      }
+    });
+});
 
 //Pag add la ine san question/answers
 app.put("/record", verifyToken, async (req, res) => {
-    const params = req.body;
+  const params = req.body;
+  try {
     let record = await Record.findOne({
-        exam:mongoose.Types.ObjectId(params.exam), 
-        student:mongoose.Types.ObjectId(req.user.id)
-    })
-    if(record){
-        let ifExist = false;
-        //Assuming that update data is an array of question = {question(an id), answer}
-        params.question.forEach(data => {
-            record.answers.forEach(recordAnswer => {
-                if(recordAnswer.question === data.question){
-                    recordAnswer.answer =data.answer
-                    ifExist = true
-                }
-            })
-        })
-        if(!ifExist){
-            record.answers.push({
-                question: mongoose.Types.ObjectId(params.question),
-                answer: params.answer
-            })
+      exam: objectId(params.exam),
+      student: objectId(req.user.id),
+    });
+    if (record) {
+      let ifExist = false;
+      record.answers.forEach((recordAnswer) => {
+        if (recordAnswer.question.equals(objectId(params.question))) {
+          recordAnswer.answer = params.answer;
+          ifExist = true;
         }
-        await record.save()
-        res.status(201).send({message: "Success", record: record})
-    }else{
-        res.status(400).send({message: "Record Not Found"})
+      });
+      if (!ifExist) {
+        record.answers.push({
+          question: objectId(params.question),
+          answer: params.answer,
+        });
+      }
+      await record.save();
+      res.status(201).send({ message: "Success" });
+    } else {
+      res.status(400).send({ message: "Record Not Found" });
     }
-})
-
+  } catch (error) {
+    console.log("ERR: ", error);
+  }
+});
 
 app.delete("/record", verifyToken, async (req, res) => {
-    const params = req.body;
-    let record = await Record.findOne({
-        exam:mongoose.Types.ObjectId(params.exam), 
-        student:mongoose.Types.ObjectId(req.user.id)
-    })
-    if(record){
-        await record.deleteOne({_id: mongoose.Types.ObjectId(record._id)});
-        res.status(201).send({message: "Success", record: record})
-    }else{
-        res.status(400).send({message: "Record Not Found"})
-    }  
-})
-module.exports = app
+  const params = req.body;
+  let record = await Record.findOne({
+    exam: objectId(params.exam),
+    student: objectId(req.user.id),
+  });
+  if (record) {
+    await record.deleteOne({ _id: objectId(record._id) });
+    res.status(201).send({ message: "Success", record: record });
+  } else {
+    res.status(400).send({ message: "Record Not Found" });
+  }
+});
+module.exports = app;
