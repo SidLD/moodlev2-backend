b7194c50bf4596670e8b6a9b62884c6d27bd8cce
Clint <clint.m@freshclinics.com>
2023-04-28 18:14:00 +0800

    fix: category

--- controllers/categoryControllers.js
+++ controllers/categoryControllers.js
@@ -1,157 +1,158 @@
-const express = require('express');
-const bodyParser = require('body-parser');
-const mongoose = require('mongoose');
+const express = require("express");
+const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
 const app = express();
 
-const Category = require("../schemas/categorySchema")
+const Category = require("../schemas/categorySchema");
 
 /**
  * An params id didi is '_id'.
  */
 
-const getCategory =  async (req,res) => {
-    const params = req.query;
-    //Dre dapat makita san student an log
-    try {
-        if(req.user.role === 'admin' || req.user.role === 'superadmin'){
-            Category
-                .where(params)
-                .populate({
-                    path: 'log.user',
-                    select: 'firstName lastName'
-                })
-                .exec((err, data) => {
-                    if (err) {
-                        res.status(400).send({message: "Error", err: err.message})
-                        return;
-                    }else{
-                        res.status(200).send({message: "Success", data: data})
-                        return;
-                    }
-                });
-        }else{
-            await Category
-                .where(params)
-                .then(data => {
-                    data.forEach(element => {
-                        element.log = undefined   
-                    });
-                    res.status(200).send({message: "Success", data: data})
-                    return;
-                })
-        }
-    } catch (error) {
-        res.status(500).send({message: "Error",  err: error})   
+const getCategory = async (req, res) => {
+  const params = req.query;
+  //Dre dapat makita san student an log
+  try {
+    if (req.user.role === "admin" || req.user.role === "superadmin") {
+      Category.where(params)
+        .populate({
+          path: "log.user",
+          select: "firstName lastName",
+        })
+        .exec((err, data) => {
+          if (err) {
+            res.status(400).send({ message: "Error", err: err.message });
+            return;
+          } else {
+            res.status(200).send({ message: "Success", data: data });
+            return;
+          }
+        });
+    } else {
+      await Category.where(params).then((data) => {
+        data.forEach((element) => {
+          element.log = undefined;
+        });
+        res.status(200).send({ message: "Success", data: data });
+        return;
+      });
     }
-}
+  } catch (error) {
+    res.status(500).send({ message: "Error", err: error });
+  }
+};
 /***
  * For Admin access only
  * Required Data
  * name
  */
-const createCategory = async (req,res) => {
+const createCategory = async (req, res) => {
+  try {
     const params = req.body;
-    if(req.user.role === "admin" || req.user.role === "superadmin"){
-        try {
-            
-            const newCategory = new Category({
-                name: params.name,
-                image : {
-                    data : params.img,
-                    contentType: "image/png"
-                }
-            })
-            newCategory.log.push({
-                user:  mongoose.Types.ObjectId(req.user.id),
-                detail: "Created "+params.name
-            })
-            // await newCategory.save( async (err, data)=>{
-            //     if(err){
-            //         res.status(400).send({message:"Error", error:err})
-            //         return;
-            //     }else{
-            //         res.status(200).send({message:"Success", data: data})
-            //         return;
-            //     }
-            // })
-            const data = params.img['$ngfDataUrl'];
-            res.status(200).send({message:"Success", data: newCategory.data});
-        } catch (error) {
-            res.status(403).send({message:"Success", error: error})
+    if (req.user.role === "admin" || req.user.role === "superadmin") {
+      try {
+        const newCategory = new Category({
+          name: params.name,
+          // image : {
+          //     data : params.img,
+          //     contentType: "image/png"
+          // }
+        });
+        newCategory.log.push({
+          user: mongoose.Types.ObjectId(req.user.id),
+          detail: "Created " + params.name,
+        });
+        await newCategory.save(async (err, data) => {
+          if (err) {
+            res.status(400).send({ message: "Error", error: err });
             return;
-        }
-    }else{
-        res.status(400).send({message:"Access Denied"})
+          } else {
+            res.status(200).send({ message: "Success", data: data });
+            return;
+          }
+        });
+        // const data = params.img["$ngfDataUrl"];
+        // res.status(200).send({ message: "Success", data: newCategory.data });
+      } catch (error) {
+        res.status(403).send({ message: "Success", error: error });
         return;
+      }
+    } else {
+      res.status(400).send({ message: "Access Denied" });
+      return;
     }
-}
+  } catch (error) {
+    res.status(400).send({ message: "Something went wrong", err: error });
+  }
+};
 /***
  * For Admin access only
  * Required Data
  * CategoryId
  */
-const updateCategory  = async (req,res) => {
-    const params = req.body;
-    if(req.user.role === "admin" || req.user.role === "superadmin"){
-        try {
-            await Category.findById(mongoose.Types.ObjectId(params._id))
-            .then(async (room) => {
-                if(room === null) {
-                    res.status(400).send({message:"Category Does not exist"})
-                    return;
-                }
-                else {
-                    room.name = params.name;
-                    room.log.push({
-                        user: mongoose.Types.ObjectId(req.user.id),
-                        detail: "Modified to "+params.name
-                    })
-                    await room.save(async (err, data) => {
-                        if(err) {
-                            res.status(400).send({message:"Error", error:err})
-                            return;
-                        }else {
-                            res.status(200).send({message:"Success", data: data})
-                            return;
-                        }
-                    })
-                }
-            })
-        } catch (error) {
-            res.status(400).send({message:"Error", error:error})
-            return;
-        }
-    }else{
-        res.status(400).send({message: "Access Denied"})
-        return;
-    }
-}
-const deleteCategory =  async (req,res, next) => {
-    const category = req.body;
+const updateCategory = async (req, res) => {
+  const params = req.body;
+  if (req.user.role === "admin" || req.user.role === "superadmin") {
     try {
-        if(req.user.role === "admin" || req.user.role === "superadmin"){
-            await Category.deleteOne({_id:category._id})
-             .then(data => {
-                 if(data.deletedCount === 1){
-                      res.status(200).send({message:"Success", deletedCount:data.deletedCount});
-                      return;
-                 }else{
-                     res.status(400).send({message:"Fail", deletedCount:data.deletedCount}); 
-                     return;  
-                 }
-             })
-            
-         }else{
-              res.status(400).send({message: "Access Denied"})
-              return;
-         }
+      await Category.findById(mongoose.Types.ObjectId(params._id)).then(
+        async (room) => {
+          if (room === null) {
+            res.status(400).send({ message: "Category Does not exist" });
+            return;
+          } else {
+            room.name = params.name;
+            room.log.push({
+              user: mongoose.Types.ObjectId(req.user.id),
+              detail: "Modified to " + params.name,
+            });
+            await room.save(async (err, data) => {
+              if (err) {
+                res.status(400).send({ message: "Error", error: err });
+                return;
+              } else {
+                res.status(200).send({ message: "Success", data: data });
+                return;
+              }
+            });
+          }
+        }
+      );
     } catch (error) {
-        res.status(500).send({message: "Error", err: error})
+      res.status(400).send({ message: "Error", error: error });
+      return;
     }
-}
-
+  } else {
+    res.status(400).send({ message: "Access Denied" });
+    return;
+  }
+};
+const deleteCategory = async (req, res, next) => {
+  const category = req.body;
+  try {
+    if (req.user.role === "admin" || req.user.role === "superadmin") {
+      await Category.deleteOne({ _id: category._id }).then((data) => {
+        if (data.deletedCount === 1) {
+          res
+            .status(200)
+            .send({ message: "Success", deletedCount: data.deletedCount });
+          return;
+        } else {
+          res
+            .status(400)
+            .send({ message: "Fail", deletedCount: data.deletedCount });
+          return;
+        }
+      });
+    } else {
+      res.status(400).send({ message: "Access Denied" });
+      return;
+    }
+  } catch (error) {
+    res.status(500).send({ message: "Error", err: error });
+  }
+};
 
 exports.createCategory = createCategory;
-exports.getCategory  = getCategory;
-exports.updateCategory   = updateCategory;
+exports.getCategory = getCategory;
+exports.updateCategory = updateCategory;
 exports.deleteCategory = deleteCategory;
