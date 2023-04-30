a14c920358fe01360688bec7eb0a3748cc5ddc08
Sid <cidlibril5@gmail.com>
2023-04-30 21:01:12 +0800

    Fix Forecasting

--- Utilities/Forecast.js
+++ Utilities/Forecast.js
@@ -3,7 +3,7 @@
         let bestAlpha = 0.0;
         let bestMAE = 0.0;
         const numOfForecast = 2;
-        for (let alpha = 0.1; alpha <= 1.0; alpha += 0.1) {
+        for (let alpha = 0.1; alpha < 1.0; alpha += 0.1) {
             let forecastValues = forecastOne(data, alpha, numOfForecast);
             let mae = calculateMae(data, forecastValues);
             if (mae < bestMAE) {
@@ -11,11 +11,34 @@
                 bestAlpha = alpha;
             }
         }
-        let finalForecast = forecastOne(data, bestAlpha, numOfForecast);
+        let finalForecast = forecastFinal(data, bestAlpha, numOfForecast);
         const forecastedData = finalForecast[finalForecast.length -1 ];
         return forecastedData;
     }
 
+    const forecastFinal = (data, alpha, numOfForecast) => {
+        let n = data.length;
+        let forecastResult = [];
+
+        forecastResult[0] = data[0];
+        //get Previous Data
+        for (let index = 1; index < n; index++) {
+           forecastResult.push({
+                date: data[index].date,
+                score: data[index].score
+           })
+        }
+
+        //Forecasting
+        for (let index = n; index < n + numOfForecast; index++) {
+            forecastResult.push({
+                date: "Forecast",
+                score: alpha * forecastResult[index-1].score + (1 - alpha) * data[index-n].score
+           })
+   
+        }
+        return forecastResult;
+    }
 
     const forecastOne = (data, alpha, numOfForecast) => {
         let n = data.length;
